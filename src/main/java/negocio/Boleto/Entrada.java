0cb0bfe9f3a9cf8316de7995c1568135fe28938a
NataliaWRamirez <wandyuxxd@gmail.com>
2021-08-24 18:53:43 -0300

    Modificaciones finales

--- src/main/java/negocio/Boleto/Entrada.java
+++ src/main/java/negocio/Boleto/Entrada.java
@@ -1,5 +1,9 @@
 package negocio.Boleto;
 
 public class Entrada extends EstadoBoleto {
-
+   private int descuentProductos;
+   public void aplicarDescuento(int descuento)
+   {
+       this.descuentProductos= descuento;
+   }
 }
