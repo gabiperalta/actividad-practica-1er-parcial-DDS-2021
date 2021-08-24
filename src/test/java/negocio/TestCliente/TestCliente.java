05728ea89699a99d2a68ab5b0d8a59e7c608bf59
gabiperalta <gabiperalta16398@gmail.com>
2021-08-24 04:56:04 -0300

    prueba TestCliente con la BBDD

--- src/test/java/negocio/TestCliente/TestCliente.java
+++ src/test/java/negocio/TestCliente/TestCliente.java
@@ -7,11 +7,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 import static negocio.Cine.Dia.Martes;
 
@@ -25,22 +22,13 @@ public class TestCliente {
     public void TestPedirEntrada(){
         Cliente cliente = new Cliente("Natalia","Ramirez");
         Date fechaTransmision = new Date(2021,12,12,16,30,00);
-        //List<String> generos =new ArrayList<>();
-        //generos.add("Drama");
         String generos = "Drama";
-        //List<String> actores =new ArrayList<>();
-        //actores.add("ActorA");
         String actores = "ActorA";
         Date estreno = new Date();
         Pelicula pelicula = new Pelicula("HP",generos,actores,estreno);
         HashMap<String,Boolean> ubicaciones = new HashMap<String,Boolean>()
         {{
             put("M9", true);
-            put("M10", false);
-        }};
-        HashMap<String,Boolean> ubicaciones2= new HashMap<String,Boolean>()
-        {{
-            put("M8", true);
             put("M10", false);
         }};
         Funcion funcion = new Funcion(23,100,100,200,Martes,fechaTransmision,pelicula);
